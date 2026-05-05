1612e47643985eceab76e3e5c924a7338b9552e5
lordluci4er <lordluci4er@gmail.com>
2026-05-06 00:35:49 +0530

    update

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,15 @@
-// app.js
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+/// Middlewares
+app.use(cors());
+app.use(express.json());
+
+/// Test Route
+app.get("/", (req, res) => {
+  res.send("MOSM Backend Running 🚀");
+});
+
+export default app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,14 @@
-// server.js
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+/// Connect DB
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
